cba44d1bdcf0d4a3a30c6850491eee0fa3a8511c
Daniel Boggs <daniel.boggs@gmail.com>
2019-07-30 15:17:29 -0400

    chore: clean up routing and items view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
   resources :items
+  get "items/:id/viewer", to: "viewer#show", as: "item_viewer"
 
-  get "items/viewer/:id", to: "viewer#show", as: "item_viewer"
+
+  root to: redirect('/items')
 
   # api_version(:module => "v0.1.0", :header => {:name => "Accept", :value => "application/iiif.nara.gove; version=1"}) do
   api_version(:module => "V1", :path => {:value => "v1"}, :defaults => {:format => :json}) do
