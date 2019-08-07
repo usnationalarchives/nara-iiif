28dfcca8b91ebc81d5a4a4a2462cd219d7a73a2b
Mat Brady <matbr8dy@gmail.com>
2019-08-07 15:20:16 -0400

    chore: adding commnets to code

--- app/controllers/v1/manifests_controller.rb
+++ app/controllers/v1/manifests_controller.rb
@@ -5,6 +5,7 @@ class V1::ManifestsController < ApplicationController
     @manifests = Item.all
   end
 
+  # Constuct a manifest from the requested Item record
   def show
     seed = {
       "@id" => helpers.v1_manifest_url(@item.naId),
@@ -17,12 +18,17 @@ class V1::ManifestsController < ApplicationController
       "viewingDirection" => "left-to-right",
       "viewingHint" => "paged"
     }
-    # Any options you add are added to the object
+
+    # Assign vales to the top level manifest object
     manifest = IIIF::Presentation::Manifest.new(seed)
 
+    # Create an instance of a sequence
     sequence = IIIF::Presentation::Sequence.new()
 
+    # Iterate over all the Record Objects associated with the Item to 
+    # create a canvas in the manifest
     @item.record_objects.each do |record_object|
+      # Create a new canvas instance
       canvas = IIIF::Presentation::Canvas.new()
       # All classes act like `ActiveSupport::OrderedHash`es, for the most part.
       # Use `[]=` to set JSON-LD properties...
@@ -30,12 +36,18 @@ class V1::ManifestsController < ApplicationController
       # ...but there are also accessors and mutators for the properties mentioned in
       # the spec
 
+      # Construct the Image info.json url
       iiif_url = URI("#{helpers.iiif_id_url(record_object.image)}/info.json")
+      # Fetch the Image info.json
+      # NOTE: this is a workaround for the limitiation described below
       iiif_json = Net::HTTP.get(iiif_url)
+      # ActiveStorage is not able to read metadata from non image resources
+      # Instead, we read the value defined by the Image API info.json
       canvas.width = JSON.parse(iiif_json)['width'] rescue 100
       canvas.height = JSON.parse(iiif_json)['height'] rescue 100
       canvas.label = record_object.label
 
+      # Insert each Record Object as an image reference within the canvas
       canvas.images << IIIF::Presentation::Annotation.new(
         # '@id' => "#{ENV.fetch("IMAGE_API_URL")}/item/2/#{record_object.id}",
         'on' => "#{ENV.fetch("IMAGE_API_URL")}/item/2/#{record_object.id}",
@@ -53,18 +65,17 @@ class V1::ManifestsController < ApplicationController
         )
       )
 
-      # canvas.thumbnail =  {
-      #   '@id' => helpers.iiif_url_from_params(record_object.image, { size: 100})
-      # }
-
+      # Append the canvas to the canvases sequences canvases attribute
       sequence.canvases << canvas
 
     end
+
+    # Append the sequence to the manifest
     manifest.sequences << sequence
 
-
+    # Return a JSON response of the manifest
     respond_to do |format|
-        format.json { render json: manifest.to_json(pretty: true), status: :ok}
+      format.json { render json: manifest.to_json(pretty: true), status: :ok}
     end
   end
 
